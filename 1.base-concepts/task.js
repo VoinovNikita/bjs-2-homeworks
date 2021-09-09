264c9b1189a32707f559dac737e22a6f1b4ce364
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-09-10 00:28:48 +0400

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,30 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = []; 
+  let D = Math.pow(b,2) - 4 * a * c;
+  console.log(D);
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  
+  if (D == 0) {
+    let X = -b / (2 * a);
+    arr.push(X);
+  }
+ 
+  if (D < 0) {
+    arr = [];
 }
+  return arr;
+
+}
+
+solveEquation(2, 333, 1)
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
