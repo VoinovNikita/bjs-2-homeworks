1e8136dc89118d850be8321224bcf48a11a5194d
Voinov Nikita <voinov.nikita2015@yandex.ru>
2021-09-11 23:55:57 +0400

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,75 @@
 // Задание 1
+
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0; 
+  let avg;
 
+
+  //console.log(max);
+  for (let i of arr) {
+    // console.log(i);
+    if (Math.max(i) > max){
+      max = i;
+      // console.log(i)
+    }
+    if (Math.min(i) < min){
+      min = i;
+    }
+    sum += i / arr.length;
+    // console.log(sum);
+   
+    avg = sum.toFixed(2);
+    
+    console.log(avg);
+    
+     
+  }
+  
   // Ваш код
 
-  return { min:min, max:max, avg:avg };
+  return { min:min, max:max, avg:Number(avg) }; 
 }
 
-// Задание 2
+
+// console.log(getArrayParams([99, -99, 10]));
+  
+
+
+//  Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
+ 
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+      
+  }
 
-  // Ваш код
-
-  return sum;
+ return sum;
 }
+
 
 function makeWork(arrOfArr, func) {
-  let max
+  
+  let max = -Infinity;
+  
+  
+  for (let n = 0; n < arrOfArr.length; n++) {
+    
+    func(arrOfArr[n]);
+     if (func(arrOfArr[n]) > max) {
+       max = func(arrOfArr[n]);
+     }
+    
+  };
 
-  // Ваш кода
-  // for ...
   
   return max
 }
-
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+// console.log(makeWork([[1,2,3],[4,5,6]], worker));
+// makeWork([[10,10,20],[20,19,10]],worker);
+// // Задание 3
+// function worker2(arr) {
+//   // Ваш код
+// }
