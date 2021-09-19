7cd7eba926815cf0375a7015c97e019c6c8591d5
Voinov Nikita <voinov.nikita2015@yandex.ru>
2021-09-19 23:37:18 +0400

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
+// let student1 = new Student("Никита", "Муж", "25");
+// let student2 = new Student("Даша", "Жен", "25")
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ 
+  this.subject = subjectName;
+}
+Student.prototype.addMark = function(mark) {
+  
+  if (this.marks === undefined) { 
+     this.marks = [mark];
+     
+    } 
+    else {
+      this.marks.push(mark);
+   }
+}
+Student.prototype.getAverage = function() {
+  let avg = 0;
+  
+  this.marks.forEach((mark) => {avg += mark});
+  return avg / this.marks.length;
+  // for (let prop in this.marks) {
+  //   console.log(prop);
+  // }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...marks) {
+  this.marks = marks;
+
+}
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+
+}
+
